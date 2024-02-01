2ab835fcd6438388c135755bbd029a884a16bfe1
Choi Yuri <yurifrom1123@gmail.com>
2024-02-02 01:38:58 +0900

    - 상세 페이지 웹툰 상세정보 가져오기 - 상세 페이지 웹툰 에피소드 정보 가져오기 - episode, detail model 생성

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:webtoonapp/models/webtoon_detail_model.dart';
+import 'package:webtoonapp/models/webtoon_episode_model.dart';
+import 'package:webtoonapp/services/api_service.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   final String title, thumb, id;
 
   DetailScreen(
       {super.key, required this.title, required this.thumb, required this.id});
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  late Future<WebtoonDetailModel> webtoon;
+  late Future<List<WebtoonEpisodeModel>> episodes;
+
+  @override
+  void initState() {
+    super.initState();
+    webtoon = ApiService.getToonById(widget.id);
+    episodes = ApiService.getLatestEpisodesById(widget.id);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,46 +34,97 @@ class DetailScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         // foregroundColor: Colors.green,
         title: Text(
-         title,
+          widget.title,
           style: TextStyle(
               color: Colors.green, fontSize: 23, fontWeight: FontWeight.w700),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 50,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
             children: [
-              Hero(
-                tag: id,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      boxShadow: [
-                        BoxShadow(
-                            blurRadius: 7,
-                            offset: Offset(5, 5),
-                            color: Colors.black.withOpacity(0.5)),
-                      ]),
-                  child: Image.network(thumb, headers: const {
-                    "User-Agent":
-                    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                  }),
-                ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Hero(
+                    tag: widget.id,
+                    child: Container(
+                      width: 250,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15),
+                          boxShadow: [
+                            BoxShadow(
+                                blurRadius: 7,
+                                offset: Offset(5, 5),
+                                color: Colors.black.withOpacity(0.5)),
+                          ]),
+                      child: Image.network(widget.thumb, headers: const {
+                        "User-Agent":
+                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                      }),
+                    ),
+                  ),
+                ],
               ),
+              const SizedBox(
+                height: 20,
+              ),
+              FutureBuilder(
+                  future: webtoon,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(snapshot.data!.about),
+                          const SizedBox(
+                            height: 15,
+                          ),
+                          Text('${snapshot.data!.genre} / ${snapshot.data!.age}'),
+                        ],
+                      );
+                    }
+                    return Text("...");
+                  }),
+              const SizedBox(
+                height: 20,
+              ),
+              FutureBuilder(
+                  future: episodes,
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      return Column(
+                        children: [
+                          for(var episode in snapshot.data!) Container(
+                            margin: EdgeInsets.only(bottom: 10),
+                            decoration: BoxDecoration(
+                              color: Colors.green.shade200,
+                              borderRadius: BorderRadius.circular(10)
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.symmetric(horizontal: 20,vertical: 10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(episode.title,style: TextStyle(color: Colors.white,fontSize: 16),),
+                                  Icon(Icons.chevron_right_outlined,color: Colors.white)
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    } else if(snapshot.hasError) {
+                      return Text('에러났어 뭐가 문제임');
+                    }
+                    return Container();
+                  })
+
             ],
           ),
-          SizedBox(
-            height: 20,
-          ),
-          Text(
-            title,
-            style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-          ),
-        ],
+        ),
       ),
     );
   }
